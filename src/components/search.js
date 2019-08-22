bab7f0a2a552c23f457d826ad8253dfddcca7f02
Alex48Yakunin <39828189+Alex48Yakunin@users.noreply.github.com>
2019-08-22 12:57:51 +0300

    Update search.js

--- src/components/search.js
+++ src/components/search.js
@@ -4,8 +4,7 @@ const templateSearch = () => {
     type="text"
     id="search__input"
     class="search__input"
-    placeholder="START TYPING — SEARCH BY WORD, #HASHTAG OR DATE"
-  />
+    placeholder="START TYPING — SEARCH BY WORD, #HASHTAG OR DATE"/>
   <label class="visually-hidden" for="search__input">Search</label>
   </section>`;
 };
