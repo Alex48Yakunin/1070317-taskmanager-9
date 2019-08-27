26b42e83c459f184feba50aefe9cc50eefbfdc87
Alex48Yakunin <39828189+Alex48Yakunin@users.noreply.github.com>
2019-08-27 14:38:37 +0300

    Добавил классы для карточки

--- src/main.js
+++ src/main.js
@@ -1,3 +1,53 @@
-import {renderTemplate} from './components/renderTemplate.js';
+import {Task} from './components/task';
+import {TaskEdit} from './components/editTask';
+import {getTask} from './components/data';
+import {Position, render} from './components/utils';
 
-renderTemplate();
+
+const TASK_COUNT = 3;
+
+const renderTask = (taskMock) => {
+  const task = new Task(taskMock);
+  const taskEdit = new TaskEdit(taskMock);
+
+  const onEscKeyDown = (evt) => {
+    if (evt.key === `Escape` || evt.key === `Esc`) {
+      tasksContainer.replaceChild(task.getElement(), taskEdit.getElement());
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    }
+  };
+
+  task.getElement()
+    .querySelector(`.card__btn--edit`)
+    .addEventListener(`click`, () => {
+      tasksContainer.replaceChild(taskEdit.getElement(), task.getElement());
+      document.addEventListener(`keydown`, onEscKeyDown);
+    });
+
+  taskEdit.getElement().querySelector(`textarea`)
+    .addEventListener(`focus`, () => {
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    });
+
+  taskEdit.getElement().querySelector(`textarea`)
+    .addEventListener(`blur`, () => {
+      document.addEventListener(`keydown`, onEscKeyDown);
+    });
+
+  taskEdit.getElement()
+    .querySelector(`.card__save`)
+    .addEventListener(`click`, () => {
+      tasksContainer.replaceChild(task.getElement(), taskEdit.getElement());
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    });
+
+  render(tasksContainer, task.getElement(), Position.BEFOREEND);
+};
+
+const taskMocks = new Array(TASK_COUNT)
+                .fill(``)
+                .map(getTask);
+
+const tasksContainer = document.querySelector(`.board__tasks`);
+
+taskMocks.forEach((taskMock) => renderTask(taskMock));
